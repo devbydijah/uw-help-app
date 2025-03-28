9ddb7459531bdd4c6ba628564e8c534b755e976b
devbydijah <k.kabir@devwithdijah.com>
2025-03-28 18:30:46 +0100

    Refactor import paths for authentication components and add UW logo image

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Onboarding1 from "./components/SplashScreen/Onboarding1";
 import Onboarding2 from "./components/SplashScreen/Onboarding2";
 import Onboarding3 from "./components/SplashScreen/Onboarding3";
-import Register from "./components/authentication/Register";
-import SignUpAs from "./components/authentication/SignUpAs"; 
-import Login from "./components/authentication/Login";
+import Register from "./components/Authentication/Register";
+import SignUpAs from "./components/Authentication/SignUpAs"; 
+import Login from "./components/Authentication/Login";
 
 function App() {
   return (
