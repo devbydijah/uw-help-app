e750bad2092fce1f87e53b5a231255790187fe1b
devbydijah <k.kabir@devwithdijah.com>
2025-04-03 12:32:34 +0100

    Configured Vite build settings to set chunk size warning limit and defined manual chunks for vendor libraries

--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 600, 
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+        },
+      },
+    },
+  },
 });
