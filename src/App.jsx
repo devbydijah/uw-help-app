02c22065c8d0c02beaf16a34fada37b27165bcfb
devbydijah <k.kabir@devwithdijah.com>
2025-04-03 12:46:34 +0100

    Updated favicon and title; implemented routing with basename in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Onboarding1 from "./components/SplashScreen/Onboarding1";
 import Onboarding2 from "./components/SplashScreen/Onboarding2";
 import Onboarding3 from "./components/SplashScreen/Onboarding3";
 import Register from "./components/Authentication/Register";
-import SignUpAs from "./components/Authentication/SignUpAs"; 
+import SignUpAs from "./components/Authentication/SignUpAs";
 import Login from "./components/Authentication/Login";
 import Homepage from "./components/Dashboard/Homepage";
 import Default from "./components/DisposeWaste/Default";
@@ -13,19 +13,20 @@ import PastWaste from "./components/Dashboard/PastWaste";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Onboarding1 />} />
-      <Route path="/onboarding2" element={<Onboarding2 />} />
-      <Route path="/onboarding3" element={<Onboarding3 />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/signupas" element={<SignUpAs />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/homepage" element={<Homepage />} />
-      <Route path="/default" element={<Default />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/pastWaste" element={<PastWaste />} />
-      {/* Add more routes as needed */}
-    </Routes>
+    <Router basename="/uw-help-app">
+      <Routes>
+        <Route path="/" element={<Onboarding1 />} />
+        <Route path="/onboarding2" element={<Onboarding2 />} />
+        <Route path="/onboarding3" element={<Onboarding3 />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/signupas" element={<SignUpAs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/default" element={<Default />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/pastWaste" element={<PastWaste />} />
+      </Routes>
+    </Router>
   );
 }
 
